14fd7cb0e5d783033b125249bb2b559579f23a0c
CETECOM <CETECOM@AVAWSV01LC0102.duoc.acad>
2024-06-17 10:41:06 -0400

    Se actualizo el menu para eliminar la saturación de DEF

--- menu.py
+++ menu.py
@@ -1,51 +1,25 @@
 from os import system
-def menu_principal():
-    opciones = {
-        '1': ('Opción 1', accion1),
-        '2': ('Opción 2', accion2),
-        '3': ('Opción 3', accion3),
-        '4': ('Salir', salir)
-    }
+validacion_salida = False
 
-    generar_menu(opciones, '4')
-
-def generar_menu(opciones, opcion_salida):
-    opcion = None
-    while opcion != opcion_salida:
-        system("cls")
-        mostrar_menu(opciones)
-        opcion = leer_opcion(opciones)
-        ejecutar_opcion(opcion, opciones)
-        print() # se imprime una línea en blanco para clarificar la salida por pantalla
-
-def mostrar_menu(opciones):
-    print('Seleccione una opción:')
-    for clave in sorted(opciones):
-        print(f' {clave}) {opciones[clave][0]}')
-
-def leer_opcion(opciones):
-    while (a := input('Opción: ')) not in opciones:
-        print('Opción incorrecta, vuelva a intentarlo.')
-    return a
-
-def ejecutar_opcion(opcion, opciones):
-    opciones[opcion][1]()
-
-
-def accion1():
-    print('Has elegido la opción 1')
-
-
-def accion2():
-    print('Has elegido la opción 2')
-
-
-def accion3():
-    print('Has elegido la opción 3')
-
-
-def salir():
-    print('Saliendo')
-
-
-menu_principal()
+while validacion_salida == False:
+    print("Seleccione una opción: ")
+    print("1-) ")
+    print("2-) ")
+    print("3-) ")
+    print("4-) ")
+    try:
+        opcion = int(input(">>> "))
+        match opcion:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                print("Adíos...")
+                validacion_salida = True
+            case _:
+                print("Opción no válida.")
+    except ValueError:
+        print("El valor debe ser un número")
